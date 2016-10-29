77da52e26064436342e42138572a7ced59f52b9c
394286006 <freemanfreelift@gmail.com>
2016-10-29 08:11:15 +0800

    workflow

--- app/routes.js
+++ app/routes.js
@@ -6,8 +6,11 @@ import Home from './p/minn/components/Home';
 import UserLogin from './p/minn/security/UserLogin';
 import AccountMngPanel from './p/minn/components/privilege/AccountMngPanel';
 import MenuMngPanel from './p/minn/components/privilege/MenuMngPanel';
+import DepartmentMngPanel from './p/minn/components/privilege/DepartmentMngPanel';
 import RoleMngPanel from './p/minn/components/privilege/RoleMngPanel';
 import DicMngPanel from './p/minn/components/privilege/DicMngPanel';
+import WorkFlowConfigMngPanel from './p/minn/components/workflow/WorkFlowConfigMngPanel';
+import LeaveProcessMngPanel from './p/minn/components/workflow/LeaveProcessMngPanel';
 
  
 export default (
@@ -15,7 +18,10 @@ export default (
     <Route path={MainConstant.app} component={Home} />
     <Route path={MainConstant.app+'/account'} component={AccountMngPanel} />
     <Route path={MainConstant.app+'/menu'} component={MenuMngPanel} />
+    <Route path={MainConstant.app+'/department'} component={DepartmentMngPanel} />
     <Route path={MainConstant.app+'/role'} component={RoleMngPanel} />
     <Route path={MainConstant.app+'/dic'} component={DicMngPanel} />
+    <Route path={MainConstant.app+'/workflowconfig'} component={WorkFlowConfigMngPanel} />
+    <Route path={MainConstant.app+'/leaveprocess'} component={LeaveProcessMngPanel} />
   </Route>
 );
